3d40a5b1e8dcaaadf10b3b103bfb6e96b2d58bfa
Tabinaadeliarafa <tabinaadeliarafa2004@gmail.com>
2025-12-04 19:13:06 +0700

    update json

--- config/cors.php
+++ config/cors.php
@@ -17,6 +17,7 @@ return [
         'http://127.0.0.1:5173',
         'http://127.0.0.1:8000',
         'http://teraz.test',
+        'https://arzetta-coliving.up.railway.app'
     ],
 
     'allowed_origins_patterns' => [],
